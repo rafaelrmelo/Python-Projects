404584dafa6605ff8e954e19e488ad5ee54e383e
Rafael Melo de Almeida <meloalmeidarafa@gmail.com>
2022-12-06 20:26:17 -0300

    alguns testes

---
+++ Calc.py
@@ -0,0 +1,7 @@
+print('\tCalculadora')
+numero1 = 50 # entrada de numero 1
+numero2 = 50 # entrada de numero 2
+numero3 = 2  # entrada de numero 3
+
+resultado = (numero1 + numero2) / numero3 * 2
+print(resultado)
---
+++ Variaveis.py
@@ -0,0 +1,20 @@
+nome = input('qual o seu nome ?: ')
+idade = input('qual a sua idade ?: ')
+altura = input('qual a sua altura ?: ')
+estuda = input('em qual escola você estuda ?: ')
+mora = input('onde você mora ?: ')
+Cpf = input('Qual o seu CPF ?: ')
+
+print(nome, 'tem', idade, 'anos e', altura, 'de altura estuda na escola', estuda, 'mora na', mora, 'seu Cpf é', Cpf)
+sim = input('voçê confere essas informações ?')
+permite = input('permite enviar suas informações ? se não aperte Ctrl+c se sim escreva abaixo')
+
+print('OBRIGADO POR FAZER ESSE FORMMULARIO, VAMOS ENVIAR AS INFORMAÇÕES.....')
+
+
+
+
+
+
+
+
